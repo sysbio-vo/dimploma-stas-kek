f72ea2116e2925101814d21631fe583de9ecf091
dantistnfs <dantistnfs@gmail.com>
2019-01-27 14:56:20 +0200

    added some articles/lectures about bwt

--- bwt_test/src/main.rs
+++ bwt_test/src/main.rs
@@ -1,5 +1,4 @@
 #![allow(dead_code)]
-#![feature(duration_as_u128)]
 use std::time::Instant;
 
 #[derive(Debug, Clone, Copy, Eq, PartialEq, PartialOrd, Ord)]
@@ -29,13 +28,11 @@ fn string_to_nucleotides(input_string: &str) -> Vec<Nucleotide> {
     output_vec
 }
 
-fn main() {
-    let some_sequence = string_to_nucleotides(&"GGTAA".repeat(100));
+fn naive_bwt(some_sequence: &Vec<Nucleotide>) -> Vec<Nucleotide> {
     let sequence_length = some_sequence.len();
 
     // First step of BWT: we need to generate array of rotations
     let mut rotations = Vec::with_capacity(sequence_length);
-    let time_start = Instant::now();
     for rotation in 0..sequence_length {
         let generated_rotation = [
             &some_sequence[rotation..sequence_length],
@@ -45,14 +42,58 @@ fn main() {
         rotations.push(generated_rotation.clone());
     }
 
-    println!("Our sequence: {:?}", some_sequence);
     //println!("Rotations: {:?}", rotations);
     rotations.sort();
     //println!("Sorted rotations {:?}", rotations);
     let mut bwt = Vec::with_capacity(sequence_length);
     for rotation in rotations.iter() {
-        bwt.push(rotation.last().unwrap());
+        bwt.push(*rotation.last().unwrap());
     }
-    println!("BWT: {:?}", bwt);
+    bwt
+}
+
+fn suffix_bwt_40(sequence: &Vec<Nucleotide>) -> Vec<Nucleotide> {
+    let sequence_length = sequence.len();
+    let mut rotations = Vec::with_capacity(sequence_length);
+    for rotation in 1..sequence_length {
+        let diff = sequence_length as i64 - rotation as i64 + 40;
+        let generated_rotation = if (sequence_length - rotation) > 40 {
+            [
+                &sequence[rotation..(rotation + 20)],
+                &sequence[(rotation + 20)..(rotation + 40)],
+            ]
+            .concat()
+        } else {
+            [
+                &sequence[rotation..sequence_length],
+                &sequence[0..diff as usize],
+            ]
+            .concat()
+        };
+        rotations.push(generated_rotation.clone());
+    }
+    //println!("Rotations: {:?}", rotations);
+    //rotations.sort();
+    println!("{:?}", std::mem::size_of_val(&rotations));
+    //println!("Rotations: {:?}", rotations);
+    //rotations.sort();
+    //println!("Sorted rotations {:?}", rotations);
+    let mut bwt = Vec::with_capacity(sequence_length);
+    for rotation in rotations.iter() {
+        bwt.push(*rotation.last().unwrap());
+    }
+    bwt
+}
+
+fn main() {
+    let some_sequence = string_to_nucleotides(&"GGTAA".repeat(200_000_000));
+    println!("Sequence length :{:?}", some_sequence.len());
+    //println!("Our sequence: {:?}", some_sequence);
+    let time_start = Instant::now();
+    println!("{:?}", &some_sequence[0..10]);
+    //let result = suffix_bwt_40(&some_sequence);
+    std::thread::sleep(std::time::Duration::from_secs(20));
+    //let result = naive_bwt(&some_sequence);
+    //println!("BWT: {:?}", result);
     println!("Time took millis: {:?}", time_start.elapsed().as_millis());
 }
